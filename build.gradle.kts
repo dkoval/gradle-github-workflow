8798f2e1751c0c1ab956d6b427c4d574ac3e76f5
dkoval <dmytro.koval@glia.com>
2024-08-28 13:36:43 +0200

    Create a new Git tag on project version change

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,7 @@
-import groovy.lang.Closure
-
 plugins {
     kotlin("jvm") version "2.0.20"
-    id("com.palantir.git-version") version "3.1.0"
     distribution
 }
-
-// https://github.com/palantir/gradle-git-version?tab=readme-ov-file#usage
-val gitVersion: Closure<String> by extra
-
-group = "com.github.dkoval"
-version = gitVersion()
 
 repositories {
     mavenCentral()
